7dabe7443424578ddf419bff245b69963e59ea4b
anshika1603 <anshika.srivastava@nashtechglobal.com>
2025-09-24 11:24:26 +0530

    Add sample application

---
+++ Security_Compliance_workshop-anshika-srivastava/day1/gitleaksDemoApp/src/main/java/com/nashtech/gitleaksDemoApp/GitleaksDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.nashtech.gitleaksDemoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitleaksDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitleaksDemoAppApplication.class, args);
+	}
+
+}
---
+++ Security_Compliance_workshop-anshika-srivastava/day1/gitleaksDemoApp/src/test/java/com/nashtech/gitleaksDemoApp/GitleaksDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nashtech.gitleaksDemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitleaksDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
